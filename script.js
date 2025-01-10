53d0601406bb351ca594db2a3990b95c70390f12
Gustavo <gustavoomonteiroo4@hotmail.com>
2025-01-10 15:45:48 -0300

    correções da foto

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", " ./assets/Light-mode.png")
+    img.setAttribute("src", " ./assets/Light-mode.jpeg")
     img.setAttribute("alt", "minha foto sem camisa")
   } else {
-    img.setAttribute("src", "./assets/Dark-mode.png")
+    img.setAttribute("src", "./assets/Dark-mode.jpeg")
     img.setAttribute(
       "alt",
       "Minha foto sem camisa e de oculos escuro"
